9497e41e41dc211afe2593444e2eb0ba06080819
DylanPJackson <frprdjackson@gmail.com>
2021-03-18 23:35:47 -0400

    Updates to formatting and planning for imp

--- code/prioritization_function_test.py
+++ code/prioritization_function_test.py
@@ -1,32 +1,57 @@
-import numpy as np
+"""
+	filename
+		prioritization_function_test.py
+
+	description
+		Currently just testing grounds to implement the prioritization function
+		and the accompanying knapsack algorithm.
+
+	author
+		Dylan P. Jackson (original contributor)
+"""
 
 class Reminder:
 	"""
 		Basic class to encapsulate the data for a Reminder as it is stored 
 		in the universe database
 	"""
-	def __init__(self, description, expiration, complete_time, creation_date):
+	def __init__(self, r_id, description, expiration, complete_time, creation_date):
+		self.r_id = r_id
 		self.description = description
 		self.expiration = expiration
 		self.complete_time = complete_time
 		self.creation_date = creation_date
 
-def prioritize(num_r, exp_dates, cre_dates):
+def prioritize(reminders):
 	"""
 		Generates value for each reminder based off of their expiration and
 		creation dates
 
 		Parameters
 		----------
-		num_r : int
-			The number of reminders at the moment
-		exp_dates : list<(id, datetime)>
-			The expiration dates associated with each reminder
-		cre_dates : list<(id, datetime)>
-			The creation dates associated with each reminder
+		reminders : list<Reminder>
 		
 		Returns
 		-------
-		list<(id, int)>
-			The prioritized value of each reminder
+		dict<int (r_id) : int (priority)>
+			Dictionary mapping reminder_id to their priority
 	"""
+	
+	"""
+		What we're actually going to do
+		- Take in a list of reminders as input, and that's it
+		- Make hash table which maps reminder ID to priority value
+		- Make two lists, each with Reminder ID and creation date / expiration date respectively
+		- Sort those two lists based off of the dates
+		- Calculate the priority based off of these sorted values
+		- Put priorities into hash table, associated with ID's
+		- Return hash table?
+	"""
+	# Dictionary mapping reminder_id's to their priorities
+	priorities = {}
+	# 2d list of reminder_id and expiration date 
+	expirations = []
+	# 2d list of reminder_id and creation date 
+	creations = []
+	
+
